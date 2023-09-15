2d37611e1aa783cd1fcef7477f709957b4384089
Pradeep Lakkineni <pradeepl@infinite.com>
2023-09-15 09:23:43 +0530

    first commit

---
+++ swapping/src/com/Infinte/Interfaces/Ecommerce.java
@@ -0,0 +1,14 @@
+package com.Infinte.Interfaces;
+
+/**
+ * @author pradeepl
+ * this interface is entry point for the ecommerce app
+ *
+ */
+public interface Ecommerce {
+	public void samsungmobile();
+	public void lgmobile();
+	public void jiomobile();
+	public void applemobile();
+
+}
